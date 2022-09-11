f1c9aa9d327475f1de4b1b78812207a806c0e5be
S. Yamaguchi <sari.yamaguchi@optim.co.jp>
2022-09-11 18:57:12 +0900

    Check the permission of post notification.
    @see https://developer.android.com/training/permissions/requesting?hl=ja
    @see https://developer.android.com/about/versions/11/privacy/permissions?hl=ja#auto-reset

--- app/src/main/java/jp/co/optim/techblog_android_notification_sample/activity/MainActivity.kt
+++ app/src/main/java/jp/co/optim/techblog_android_notification_sample/activity/MainActivity.kt
@@ -1,9 +1,14 @@
 package jp.co.optim.techblog_android_notification_sample.activity
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import jp.co.optim.techblog_android_notification_sample.databinding.ActivityMainBinding
 import jp.co.optim.techblog_android_notification_sample.extension.TAG
 import jp.co.optim.techblog_android_notification_sample.extension.logI
@@ -15,6 +20,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private val notificationPostman = NotificationPostman()
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            if (isGranted) {
+                logI("The permission is granted just now.")
+            } else {
+                logW("The permission is denied.")
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,22 +43,51 @@ class MainActivity : AppCompatActivity() {
         }
         binding.buttonPostNotification.setOnClickListener {
             logI("Clicked postNotification.")
-            notificationPostman.post(this)
+            if (checkNotificationPermission()) {
+                notificationPostman.post(this)
+            }
         }
         binding.buttonPostCallNotification.setOnClickListener {
             logI("Clicked postCallNotification.")
-            notificationPostman.postCall(this)
+            if (checkNotificationPermission()) {
+                notificationPostman.postCall(this)
+            }
         }
         binding.buttonPostNewCallNotification.let { btn ->
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
                 btn.setOnClickListener {
                     logI("Clicked postNewCallNotification.")
-                    notificationPostman.postNewCall(this)
+                    if (checkNotificationPermission()) {
+                        notificationPostman.postNewCall(this)
+                    }
                 }
             } else {
                 logW("Cannot use postNewCallNotification. OS version is less than API31.")
                 btn.isEnabled = false
             }
         }
+    }
+
+    private fun checkNotificationPermission(): Boolean {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            logI("No needed the permission.")
+            return true
+        }
+        if (ContextCompat.checkSelfPermission(
+                this, Manifest.permission.POST_NOTIFICATIONS)
+            == PackageManager.PERMISSION_GRANTED) {
+            logI("The permission is granted already.")
+            return true
+        }
+        logI("The permission is not granted yet.")
+        if (ActivityCompat.shouldShowRequestPermissionRationale(
+                this, Manifest.permission.POST_NOTIFICATIONS)) {
+            logI("App should show the request of permission rationale.")
+            // TODO: Must be implemented here!
+            return false
+        }
+        logI("Launch the request of the permission.")
+        requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        return false
     }
 }
