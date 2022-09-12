525f9f309edd1461b08d7e4849619e83aa99e897
S. Yamaguchi <sari.yamaguchi@optim.co.jp>
2022-09-12 17:35:13 +0900

    Update UI.
    * Show the alert dialog for the request of permission rationale.
    * Show the snackbar for the result of the permission request.

--- app/src/main/java/jp/co/optim/techblog_android_notification_sample/activity/MainActivity.kt
+++ app/src/main/java/jp/co/optim/techblog_android_notification_sample/activity/MainActivity.kt
@@ -7,27 +7,32 @@ import android.os.Build
 import android.os.Bundle
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import jp.co.optim.techblog_android_notification_sample.R
 import jp.co.optim.techblog_android_notification_sample.databinding.ActivityMainBinding
 import jp.co.optim.techblog_android_notification_sample.extension.TAG
 import jp.co.optim.techblog_android_notification_sample.extension.logI
 import jp.co.optim.techblog_android_notification_sample.extension.logW
+import jp.co.optim.techblog_android_notification_sample.fragment.AlertDialogFragment
 import jp.co.optim.techblog_android_notification_sample.notification.NotificationPostman
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AlertDialogFragment.Callback {
 
     private lateinit var binding: ActivityMainBinding
 
     private val notificationPostman = NotificationPostman()
 
     private val requestPermissionLauncher =
-        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
-            if (isGranted) {
-                logI("The permission is granted just now.")
-            } else {
-                logW("The permission is denied.")
-            }
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+            Snackbar.make(
+                binding.root,
+                getString(
+                    if (it) R.string.granted_permission_snackbar_message
+                    else R.string.denied_permission_snackbar_message
+                ),
+                Snackbar.LENGTH_LONG
+            ).show()
         }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,6 +73,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onDismissed(tag: String?) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            throw InternalError("Conflicted OS version.")
+        }
+        logI("Launch the request of the permission.")
+        requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+    }
+
     private fun checkNotificationPermission(): Boolean {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
             logI("No needed the permission.")
@@ -79,11 +92,13 @@ class MainActivity : AppCompatActivity() {
             logI("The permission is granted already.")
             return true
         }
-        logI("The permission is not granted yet.")
-        if (ActivityCompat.shouldShowRequestPermissionRationale(
-                this, Manifest.permission.POST_NOTIFICATIONS)) {
+        if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
             logI("App should show the request of permission rationale.")
-            // TODO: Must be implemented here!
+            AlertDialogFragment.newInstance(
+                getString(R.string.request_permission_dialog_title),
+                getString(R.string.request_permission_dialog_message),
+                getString(R.string.ok)
+            ).show(supportFragmentManager, null)
             return false
         }
         logI("Launch the request of the permission.")
---
+++ app/src/main/java/jp/co/optim/techblog_android_notification_sample/fragment/AlertDialogFragment.kt
@@ -0,0 +1,60 @@
+package jp.co.optim.techblog_android_notification_sample.fragment
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.core.os.bundleOf
+import androidx.fragment.app.DialogFragment
+
+class AlertDialogFragment : DialogFragment() {
+
+    interface Callback {
+        fun onDismissed(tag: String?)
+    }
+
+    companion object {
+        private const val ARGS_TITLE = "title"
+        private const val ARGS_MESSAGE = "message"
+        private const val ARGS_BTN_TEXT = "btn_text"
+
+        fun newInstance(
+            title: String? = null,
+            message: String? = null,
+            btnText: String? = null,
+            cancelable: Boolean = false
+        ): DialogFragment = AlertDialogFragment().apply {
+            arguments = bundleOf(
+                Pair(ARGS_TITLE, title),
+                Pair(ARGS_MESSAGE, message),
+                Pair(ARGS_BTN_TEXT, btnText),
+            )
+            isCancelable = cancelable
+        }
+    }
+
+    private val args: Bundle by lazy {
+        arguments ?: throw IllegalArgumentException("Not found arguments.")
+    }
+    private val title: String? by lazy { args.getString(ARGS_TITLE) }
+    private val message: String? by lazy { args.getString(ARGS_MESSAGE) }
+    private val btnText: String? by lazy { args.getString(ARGS_BTN_TEXT) }
+    private val callback: Callback? by lazy { requireActivity() as? Callback }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        AlertDialog.Builder(requireActivity()).apply {
+            title?.let { setTitle(it) }
+            message?.let { setMessage(it) }
+            btnText?.let {
+                setPositiveButton(it) { _, _ ->
+                    dismiss()
+                    callback?.onDismissed(tag)
+                }
+            }
+        }.create()
+
+    override fun onCancel(dialog: DialogInterface) {
+        super.onCancel(dialog)
+        callback?.onDismissed(tag)
+    }
+}
